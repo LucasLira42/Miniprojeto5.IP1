9fc6d869fd04cd1ff6b93b538b8d34705e8de6e6
Lucas Lira <lucas.velez@ufrpe.br>
2025-11-17 13:30:05 -0300

    bolinha commitou

---
+++ miniprojeto5.py
@@ -0,0 +1,42 @@
+def main():
+    return 
+pessoas = []
+
+
+laco = 0
+while laco == 0:
+
+    print("[SIMULADOR DE RELAÇÕES DO MERCADO]\n - [d]igitar dados de nova pessoa\n - [m]ostrar pessoas cadastradas\n - [s]air")
+    opcao = input("digite aqui: ")
+
+    if opcao == "d":
+        print("digitar dados de uma nova pessoa")
+        laco= 1
+
+    elif opcao == "m":
+        print("mostrar pessoas casdastradas")
+        laco=1
+
+    elif opcao == "s":
+        print("Saindo")
+        quit()
+        laco = 1
+
+    else:
+        print("opção inválida")
+        laco = 0
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
